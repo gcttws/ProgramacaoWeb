fcb171cd747b6538f1ed1ff52c6be32f25311fdd
gcttws <gustavo.capistrano64@gmail.com>
2023-11-19 01:52:25 -0300

    Iniciando produtoController

--- Projeto_Final/src/controller/produtoController.js
+++ Projeto_Final/src/controller/produtoController.js
@@ -1,4 +1,4 @@
-const Anotacao = require('../model/produto');
+const Produto = require('../model/produto');
 
 function indexView(req, res) {
     res.render('index.html');
@@ -6,21 +6,42 @@ function indexView(req, res) {
 
 function homeView(req, res) {
 
-    Anotacao.findAll({
+    Produto.findAll({
         where: {
-            id_usuario: req.session.usuario.id,
-            indicador_ativo: 1
+            usuario_id: req.session.usuario.id,
+            quantidade: {
+                [Sequelize.Op.gt] : 1 // Usando o operador 'greater than' do Sequelize
+            }
         }
-    }).then((anotacaos)=>{
-        res.render('home.html', {anotacaos});
-    }).catch((erro_recupera_anotacaos)=>{
-        res.render('home.html', {erro_recupera_anotacaos});
+    }).then((produtos)=>{
+        res.render('home.html', {produtos});
+    }).catch((erro_recupera_produtos)=>{
+        res.render('home.html', {erro_recupera_produtos});
     });
+}
 
-    
+function cadastrarProduto(req, res){
+    let produto = {
+        nome: typeof req.body.nome === 'string' ? req.body.nome : '',
+        descricao: typeof req.body.descricao === 'string' ? req.body.descricao : '',
+        quantidade: req.body.quantidade,
+        preco_custo: req.body.preco_custo,
+        preco_venda: req.body.preco_venda,
+        categoria: req.body.categoria,
+        usuario_id: req.session.usuario.id
+    }
+
+    Produto.create(produto).then(()=>{
+        res.redirect('/home');
+    }).catch((err) => {
+        console.log(err);
+        let erro_cadastrar_produto = true;
+        res.render("home.html", {erro_cadastrar_produto});
+    })
 }
 
 module.exports = {
     indexView,
-    homeView
+    homeView,
+    cadastrarProduto
 }
